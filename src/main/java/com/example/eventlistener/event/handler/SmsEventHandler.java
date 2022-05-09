4eda824eea6f5ff2837511881a5992d80646b4cd
정준영 <jeongjy@mrblue.com>
2022-05-09 18:25:26 +0900

    feat: 비동기 방식 추가

--- src/main/java/com/example/eventlistener/event/handler/SmsEventHandler.java
+++ src/main/java/com/example/eventlistener/event/handler/SmsEventHandler.java
@@ -2,18 +2,23 @@ package com.example.eventlistener.event.handler;
 
 import com.example.eventlistener.event.RegisteredEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SmsEventHandler {
 
+  @Async
   @EventListener
-  public void sendSms(RegisteredEvent event){
+  public void sendSms(RegisteredEvent event) throws InterruptedException {
+    Thread.sleep(2000);
     System.out.println(event.getName() + "님에게 가입 축하 메세지를 전송했습니다.");
   }
 
+  @Async
   @EventListener
-  public void makeCoupon(RegisteredEvent event) {
+  public void makeCoupon(RegisteredEvent event) throws InterruptedException {
+    Thread.sleep(1000);
     System.out.println(event.getName() + "님에게 쿠폰을 전송했습니다.");
   }
 }
